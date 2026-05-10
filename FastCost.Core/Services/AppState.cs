5cd3dd3a7e9fb2d6c28148680c24ae68ae1bd0c4
Karol <kaajooj@gmail.com>
2026-05-10 15:37:47 +0200

    make AppState.DataVersion increment thread-safe

--- FastCost.Core/Services/AppState.cs
+++ FastCost.Core/Services/AppState.cs
@@ -2,12 +2,11 @@ namespace FastCost.Core.Services
 {
     public static class AppState
     {
-        // Global version of data. Incremented whenever a cost is added, edited or deleted.
-        public static int DataVersion { get; private set; } = 1;
+        private static int _dataVersion = 1;
 
-        public static void NotifyDataChanged()
-        {
-            DataVersion++;
-        }
+        // Global version of data. Incremented whenever a cost is added, edited or deleted.
+        public static int DataVersion => _dataVersion;
+
+        public static void NotifyDataChanged() => Interlocked.Increment(ref _dataVersion);
     }
 }
